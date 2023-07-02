2ee1b49790e569e8ff9cd963e770ece71b8ec5ea
Takaya Saeki <abc.tkys+pub@gmail.com>
2023-07-02 21:47:45 +0900

    build only 64-bit, and no archive in build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-mkdir -p zenhan/bin64
-mkdir -p zenhan/bin32
+mkdir -p zenhan
 
-out=zenhan/bin64/zenhan.exe
-x86_64-w64-mingw32-g++-win32 -std=c++11 -mwindows main.cc -o $out -limm32
-x86_64-w64-mingw32-strip $out
-
-out=zenhan/bin32/zenhan.exe
-i686-w64-mingw32-g++-win32 -std=c++11 -mwindows -static-libgcc main.cc -o $out -limm32
-i686-w64-mingw32-strip $out
-
-zip -r9 zenhan.zip zenhan/
+out=zenhan/zenhan.exe
+x86_64-w64-mingw32-g++ -std=c++11 -mwindows main.cc -o $out -limm32
+strip $out
